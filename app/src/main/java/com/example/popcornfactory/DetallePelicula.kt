da349707fd597ab7010ed9b22fa29b2d1878c4d7
vvcrxyz <nomar.limon04@gmail.com>
2025-09-23 11:01:16 -0700

    pt1

--- app/src/main/java/com/example/popcornfactory/DetallePelicula.kt
+++ app/src/main/java/com/example/popcornfactory/DetallePelicula.kt
@@ -1,22 +1,26 @@
 package com.example.popcornfactory
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
-
 
 class DetallePelicula : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_pelicula)
 
+        // Referencias a las vistas
+        val ivPeliculaImagen = findViewById<ImageView>(R.id.iv_pelicula_imagen)
+        val tvNombrePelicula = findViewById<TextView>(R.id.iv_nombre_pelicula)
+        val tvPeliculaDesc = findViewById<TextView>(R.id.iv_pelicula_desc)
 
+        // Recuperar los datos
         val bundle = intent.extras
-
-        if(bundle != null){
-            this.iv_pelicula_imagen.setImageResorce(bundle.getInt("header"))
-            tv_nombre_pelicula.setText(bundle.getInt("nombre"))
-            tv_pelicula_desc.seText(bundle.getInt("sinopsis"))
+        if (bundle != null) {
+            ivPeliculaImagen.setImageResource(bundle.getInt("header"))
+            tvNombrePelicula.text = bundle.getString("titulo")
+            tvPeliculaDesc.text = bundle.getString("sinopsis")
         }
     }
 }
